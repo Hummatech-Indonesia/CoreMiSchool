927f9a724449450a4205b883ddd248a26bd747f8
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-05 13:03:49 +0700

    sidebar admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,11 +2,8 @@
     <!-- Sidebar scroll-->
     <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
-            <a href="index-2.html" class="text-nowrap logo-img">
-                <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/dark-logo.svg"
-                    class="dark-logo" width="180" alt="" />
-                <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/light-logo.svg"
-                    class="light-logo" width="180" alt="" />
+            <a href="/" class="text-nowrap logo-img">
+                <img src="{{ asset('assets/images/logo/logo-miscool.png') }}" width="200px" alt="">
             </a>
             <div class="close-btn d-lg-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                 <i class="ti ti-x fs-8 text-muted"></i>
--- routes/route/admin.php
+++ routes/route/admin.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/admin', function () {
     return view('admin.pages.dashboard');
 });
+
+Route::get('rfid-registration', function(){
+    return view('admin.pages.absensi.registrasi-rfid');
+});
