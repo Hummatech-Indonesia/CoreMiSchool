886255d939254121a8c7b72f4e61fdacaf5bbed6
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-05 23:54:49 +0700

    registrasi rfid admin

--- resources/views/admin/pages/absensi/registrasi-rfid.blade.php
+++ resources/views/admin/pages/absensi/registrasi-rfid.blade.php
@@ -55,7 +55,7 @@
                 <form class="d-flex gap-2">
                     <div class="position-relative">
                         <div class="">
-                            <input type="text" name="search" class="form-control search-chat py-2 px-5 ps-5" id="search-name" placeholder="Search">
+                            <input type="text" name="search" class="form-control search-chat py-2 px-5 ps-5" id="search-name" placeholder="Cari">
                             <i class="ti ti-search position-absolute top-50 translate-middle-y fs-6 text-dark ms-3"></i>
                         </div>
                     </div>
