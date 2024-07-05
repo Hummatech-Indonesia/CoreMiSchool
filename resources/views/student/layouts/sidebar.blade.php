637dd30f8ceae7a1592e30be48b62d0c506b7973
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-05 09:51:19 +0700

    student layouts

---
+++ resources/views/student/layouts/sidebar.blade.php
@@ -0,0 +1,207 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+            <a href="index-2.html" class="text-nowrap logo-img">
+                <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/dark-logo.svg"
+                    class="dark-logo" width="180" alt="" />
+                <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/light-logo.svg"
+                    class="light-logo" width="180" alt="" />
+            </a>
+            <div class="close-btn d-lg-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8 text-muted"></i>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar>
+            <ul id="sidebarnav">
+                <!-- ============================= -->
+                <!-- Home -->
+                <!-- ============================= -->
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+                <!-- =================== -->
+                <!-- Dashboard -->
+                <!-- =================== -->
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="index-2.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-aperture"></i>
+                        </span>
+                        <span class="hide-menu">Modern</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="index2.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-shopping-cart"></i>
+                        </span>
+                        <span class="hide-menu">eCommerce</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="index3.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-currency-dollar"></i>
+                        </span>
+                        <span class="hide-menu">NFT</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="index4.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cpu"></i>
+                        </span>
+                        <span class="hide-menu">Crypto</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="index5.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-activity-heartbeat"></i>
+                        </span>
+                        <span class="hide-menu">General</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="index6.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-playlist"></i>
+                        </span>
+                        <span class="hide-menu">Music</span>
+                    </a>
+                </li>
+                <!-- ============================= -->
+                <!-- Apps -->
+                <!-- ============================= -->
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Apps</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="app-contact2.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-list-details"></i>
+                        </span>
+                        <span class="hide-menu">Contact List</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="app-invoice.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-file-text"></i>
+                        </span>
+                        <span class="hide-menu">Invoice</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="page-user-profile.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-user-circle"></i>
+                        </span>
+                        <span class="hide-menu">User Profile</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow" href="#" aria-expanded="false">
+                        <span class="d-flex">
+                            <i class="ti ti-chart-donut-3"></i>
+                        </span>
+                        <span class="hide-menu">Blog</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="blog-posts.html" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Posts</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="blog-detail.html" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Details</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow" href="#" aria-expanded="false">
+                        <span class="d-flex">
+                            <i class="ti ti-basket"></i>
+                        </span>
+                        <span class="hide-menu">Ecommerce</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="eco-shop.html" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Shop</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="eco-shop-detail.html" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Details</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="eco-product-list.html" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">List</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="eco-checkout.html" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Checkout</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="unlimited-access hide-menu bg-light-primary position-relative my-7 rounded">
+                <div class="d-flex">
+                    <div class="unlimited-access-title">
+                        <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Unlimited Access</h6>
+                        <button class="btn btn-primary fs-2 fw-semibold lh-sm">Signup</button>
+                    </div>
+                    <div class="unlimited-access-img">
+                        <img src="../../dist/images/backgrounds/rocket.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="fixed-profile p-3 bg-light-secondary rounded sidebar-ad mt-3">
+            <div class="hstack gap-3">
+                <div class="john-img">
+                    <img src="../../dist/images/profile/user-1.jpg" class="rounded-circle" width="40"
+                        height="40" alt="">
+                </div>
+                <div class="john-title">
+                    <h6 class="mb-0 fs-4 fw-semibold">Mathew</h6>
+                    <span class="fs-2 text-dark">Designer</span>
+                </div>
+                <button class="border-0 bg-transparent text-primary ms-auto" tabindex="0" type="button"
+                    aria-label="logout" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="logout">
+                    <i class="ti ti-power fs-6"></i>
+                </button>
+            </div>
+        </div>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
