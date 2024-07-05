e8ffcfc7ab0c8667892ff2c49bdad1b5eb5280ff
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-05 08:59:51 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+require_once __DIR__ . '/admin.php';
+require_once __DIR__ . '/school.php';
+require_once __DIR__ . '/staff.php';
+require_once __DIR__ . '/teacher.php';
+require_once __DIR__ . '/student.php';
