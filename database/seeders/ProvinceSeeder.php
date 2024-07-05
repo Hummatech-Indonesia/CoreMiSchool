e464d086d4db46535e1edca63f425ff395c6f320
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-07-05 15:14:59 +0700

    Update ProvinceSeeder.php

--- database/seeders/ProvinceSeeder.php
+++ database/seeders/ProvinceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Province;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,45 @@ class ProvinceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $provinces = [
+            'Aceh',
+            'Sumatera Utara',
+            'Sumatera Barat',
+            'Riau',
+            'Jambi',
+            'Sumatera Selatan',
+            'Bengkulu',
+            'Lampung',
+            'Kepulauan Bangka Belitung',
+            'Kepulauan Riau',
+            'Jakarta',
+            'Jawa Barat',
+            'Jawa Tengah',
+            'Yogyakarta',
+            'Jawa Timur',
+            'Banten',
+            'Bali',
+            'Nusa Tenggara Barat',
+            'Nusa Tenggara Timur',
+            'Kalimantan Barat',
+            'Kalimantan Tengah',
+            'Kalimantan Selatan',
+            'Kalimantan Timur',
+            'Kalimantan Utara',
+            'Sulawesi Utara',
+            'Sulawesi Tengah',
+            'Sulawesi Selatan',
+            'Sulawesi Tenggara',
+            'Gorontalo',
+            'Sulawesi Barat',
+            'Maluku',
+            'Maluku Utara',
+            'Papua',
+            'Papua Barat'
+        ];
+        
+        foreach ($provinces as $province) {
+            Province::factory()->create(['name' => $province]);
+        }
     }
 }
