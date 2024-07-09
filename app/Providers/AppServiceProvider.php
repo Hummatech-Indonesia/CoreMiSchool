c61bd8e139c5486f9e588e317f9f7257053eef02
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-07-09 08:40:30 +0700

    user interface-repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,27 @@
 
 namespace App\Providers;
 
+use App\Contracts\Interfaces\StudentInterface;
+use App\Contracts\Interfaces\UserInterface;
+use App\Contracts\Repositories\StudentRepository;
+use App\Contracts\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private array $register = [
+        UserInterface::class => UserRepository::class,
+        StudentInterface::class => StudentRepository::class,
+    ];
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        foreach ($this->register as $index => $value) {
+            $this->app->bind($index, $value);
+        }
     }
 
     /**
