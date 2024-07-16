c8d63236fb6dfca16c231d2a7f113eb9e79d3c58
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-16 10:37:58 +0700

    search dan filter pegawai

--- app/Contracts/Interfaces/EmployeeInterface.php
+++ app/Contracts/Interfaces/EmployeeInterface.php
@@ -9,11 +9,12 @@ use App\Contracts\Interfaces\Eloquent\ShowWithSlugInterface;
 use App\Contracts\Interfaces\Eloquent\StoreInterface;
 use App\Contracts\Interfaces\Eloquent\UpdateInterface;
 use App\Contracts\Interfaces\Eloquent\WhereInterface;
+use Illuminate\Http\Request;
 
 interface EmployeeInterface extends GetInterface, StoreInterface, UpdateInterface, ShowInterface, DeleteInterface, WhereInterface, ShowWithSlugInterface
 {
     public function paginate($query): mixed;
-    public function whereSchool(mixed $id, $query): mixed;
+    public function whereSchool(mixed $id, $query, Request $request): mixed;
     public function getTeacherBySchool(mixed $id): mixed;
     public function getCountEmployee(mixed $query) : mixed;
     public function getSchool(mixed $id): mixed;
