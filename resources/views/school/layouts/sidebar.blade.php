8074e4e49551fd8c5109c7e3cb124ca9c33c1ddc
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-16 15:01:03 +0700

    sidebar alumni

--- resources/views/school/layouts/sidebar.blade.php
+++ resources/views/school/layouts/sidebar.blade.php
@@ -87,7 +87,7 @@
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link" href="/school/alumni" aria-expanded="false">
+                    <a class="sidebar-link" href="{{ route('class-alumni.index') }}" aria-expanded="false">
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256">
                                 <path fill="currentColor"
