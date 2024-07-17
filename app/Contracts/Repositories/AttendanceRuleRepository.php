dbb6c3d1047142e8be1edfd84e6eb35004da170d
ArdianS18 <ardiansupriadi464@gmail.com>
2024-07-17 08:16:14 +0700

    show by day

--- app/Contracts/Repositories/AttendanceRuleRepository.php
+++ app/Contracts/Repositories/AttendanceRuleRepository.php
@@ -47,8 +47,8 @@ class AttendanceRuleRepository extends BaseRepository implements AttendanceRuleI
         return $this->model->query()->where('day', $day)->where('role', $role)->firstOrFail();
     }
 
-    public function showByDay(mixed $school_id, string $day): mixed
+    public function showByDay(mixed $school_id, string $day, string $roles): mixed
     {
-        return $this->model->query()->where(['school_id' => $school_id, 'day' => $day])->first();
+        return $this->model->query()->where(['school_id' => $school_id, 'day' => $day, 'role' => $roles])->first();
     }
 }
