55ba5bde80023788b9e840523c79581c16de8520
ArdianS18 <ardiansupriadi464@gmail.com>
2024-07-17 15:55:14 +0700

    Update AttendanceRepository.php

--- app/Contracts/Repositories/AttendanceRepository.php
+++ app/Contracts/Repositories/AttendanceRepository.php
@@ -62,7 +62,7 @@ class AttendanceRepository extends BaseRepository implements AttendanceInterface
 
     public function whereClassroom(mixed $id): mixed
     {
-        return $this->model->query()->whereRelation('classroomStudent', 'classroom_id', $id)->get();
+        return $this->model->query()->whereRelation('classroomStudent', 'classroom_id', $id)->paginate(10);
     }
 
     public function classAndDate(mixed $classroom_id, Request $request): mixed
