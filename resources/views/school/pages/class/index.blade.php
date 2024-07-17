e05cf51ce4abf2985457923100de29f6b44bacbb
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-17 15:15:28 +0700

    paginate detail kelas

--- resources/views/school/pages/class/index.blade.php
+++ resources/views/school/pages/class/index.blade.php
@@ -231,7 +231,7 @@
             </div>
         @endforelse
     </div>
-    <div class="pagination justify-content-end mb-0">
+    <div class="pagination justify-content-center mb-0">
         <x-paginate-component :paginator="$classrooms" />
     </div>
 
