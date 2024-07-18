8a10b7fa763f6d4bf284a0999eb775ce6942bd82
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-07-18 15:26:41 +0700

    adding red star

--- resources/views/school/pages/settings/update-information.blade.php
+++ resources/views/school/pages/settings/update-information.blade.php
@@ -17,77 +17,77 @@
                 @csrf
                 <div class="row mt-5">
                     <div class="col-md-6 mb-4">
-                        <label for="">Nama Sekolah</label>
+                        <label for="">Nama Sekolah<span class="text-d">*</span></label>
                         <input type="text" class="form-control mt-1" placeholder="Masukan nama sekolah" name="name" value="{{ old('name', $school->user->name) }}">
                         @error('name')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-6 mb-4">
-                        <label for="">Kepala Sekolah</label>
+                        <label for="">Kepala Sekolah<span class="text-d">*</span></label>
                         <input type="text" class="form-control mt-1" placeholder="Masukan kepala sekolah" name="head_school" value="{{ old('head_school', $school->head_school) }}">
                         @error('head_school')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-6 mb-4">
-                        <label for="">Email Sekolah</label>
+                        <label for="">Email Sekolah<span class="text-d">*</span></label>
                         <input type="text" class="form-control mt-1" placeholder="Masukan email sekolah" name="email" value="{{ old('email', $school->user->email) }}">
                         @error('email')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-6 mb-4">
-                        <label for="">Telepon Sekolah</label>
+                        <label for="">Telepon Sekolah<span class="text-d">*</span></label>
                         <input type="text" name="phone_number" class="form-control mt-1" placeholder="Masukan telepon sekolah" value="{{ old('phone_number', $school->phone_number) }}">
                         @error('phone_number')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-12 mb-4">
-                        <label for="">Alamat Sekolah</label>
+                        <label for="">Alamat Sekolah<span class="text-d">*</span></label>
                         <textarea name="address" class="form-control mt-1" placeholder="Masukan alamat sekolah" rows="2">{{ old('address', $school->address) }}</textarea>
                         @error('address')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-12 mb-4">
-                        <label for="">Deskripsi Sekolah</label>
+                        <label for="">Deskripsi Sekolah<span class="text-d">*</span></label>
                         <textarea name="description" class="form-control mt-1" placeholder="Masukan deskripsi sekolah" rows="6">{{ old('description', $school->description) }}</textarea>
                         @error('description')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-6 mb-4">
-                        <label for="">Akreditasi</label>
+                        <label for="">Akreditasi<span class="text-d">*</span></label>
                         <input type="text" class="form-control mt-1" name="accreditation" placeholder="Masukan akreditasi sekolah" value="{{ old('accreditation', $school->accreditation) }}">
                         @error('accreditation')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-6 mb-4">
-                        <label for="">Website</label>
+                        <label for="">Website<span class="text-d">*</span></label>
                         <input type="text" class="form-control" name="website_school" id="" placeholder="Masukan website sekolah" value="{{ old('website_school', $school->website_school) }}">
                         @error('website_school')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-4 mb-4">
-                        <label for="">Kode Pos</label>
+                        <label for="">Kode Pos<span class="text-d">*</span></label>
                         <input type="text" class="form-control" name="pas_code" id="" placeholder="Masukan kode pos sekolah" value="{{ old('pas_code', $school->pas_code) }}">
                         @error('pas_code')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-4 mb-4">
-                        <label for="">NPSN</label>
+                        <label for="">NPSN<span class="text-d">*</span></label>
                         <input type="text" class="form-control" name="npsn" id="" placeholder="Masukan NPSN sekolah" value="{{ old('npsn', $school->npsn) }}">
                         @error('npsn')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="col-md-4 mb-4">
-                        <label for="">NIP</label>
+                        <label for="">NIP<span class="text-d">*</span></label>
                         <input type="text" class="form-control" name="nip" id="" placeholder="Masukan NIP sekolah" value="{{ old('nip', $school->nip) }}">
                         @error('nip')
                             <span class="text-danger">{{ $message }}</span>
