08e7f516537b7054dfbb72bff984aabf316cc861
nesaathussholeha <nesaathussholeha@gmail.com>
2024-07-21 22:02:26 +0700

    modal update, detai, rfid siswa

--- resources/views/school/new/class/detail.blade.php
+++ resources/views/school/new/class/detail.blade.php
@@ -60,7 +60,8 @@
                     <td>Laki - laki</td>
                     <td>2131123123</td>
                     <td>-
-                        <button type="submit" class="btn btn-rounded btn-warning ms-2 btn-rfid p-1">
+                        <button type="submit" class="btn btn-rounded btn-warning ms-2 btn-rfid p-1" data-bs-toggle="modal"
+                            data-bs-target="#rfid-student">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                 <path fill="currentColor"
                                     d="M21 12a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1m-15 .76V17a1 1 0 0 0 1 1h4.24a1 1 0 0 0 .71-.29l6.92-6.93L21.71 8a1 1 0 0 0 0-1.42l-4.24-4.29a1 1 0 0 0-1.42 0l-2.82 2.83l-6.94 6.93a1 1 0 0 0-.29.71m10.76-8.35l2.83 2.83l-1.42 1.42l-2.83-2.83ZM8 13.17l5.93-5.93l2.83 2.83L10.83 16H8Z" />
@@ -83,12 +84,14 @@
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="">
                                 <li>
                                     <button type="button" data-id="1dbf93d1-4e70-37ec-abda-b496e6a3c177"
+                                        data-bs-toggle="modal" data-bs-target="#student-detail"
                                         class="btn-detail dropdown-item d-flex align-items-center gap-3"><i
                                             class="fs-4 ti ti-eye"></i>Detail</button>
                                 </li>
                                 <li>
                                     <button type="button"
-                                        class="dropdown-item d-flex align-items-center gap-3 btn-edit-teacher">
+                                        class="dropdown-item d-flex align-items-center gap-3 btn-edit-teacher"
+                                        data-bs-toggle="modal" data-bs-target="#update-student">
                                         <i class="fs-4 ti ti-edit"></i>Edit
                                     </button>
                                 </li>
@@ -107,4 +110,7 @@
 
     @include('school.new.class.widgets.create-student')
     @include('school.new.class.widgets.import-student')
+    @include('school.new.class.widgets.update-student')
+    @include('school.new.class.widgets.rfid-student')
+    @include('school.new.class.widgets.detail-student')
 @endsection
