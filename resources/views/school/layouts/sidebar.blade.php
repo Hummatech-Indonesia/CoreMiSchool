de17b92c37161b1a2fd7a12ccb6b3e08ad76f134
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-21 15:31:25 +0700

    Update sidebar.blade.php

--- resources/views/school/layouts/sidebar.blade.php
+++ resources/views/school/layouts/sidebar.blade.php
@@ -115,7 +115,7 @@
                     <span class="hide-menu">Pengaturan</span>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link" href="{{ route('school.school-years.index') }}" aria-expanded="false">
+                    <a class="sidebar-link" href="{{ route('new.school-year.index') }}" aria-expanded="false">
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                 viewBox="0 0 24 24">
