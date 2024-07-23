8e2d7c9e4baa1b4945dfafc0fa50d385f712fb4a
nesaathussholeha <nesaathussholeha@gmail.com>
2024-07-23 14:04:32 +0700

    update tampilan kelas

--- resources/views/school/new/class/index.blade.php
+++ resources/views/school/new/class/index.blade.php
@@ -38,7 +38,8 @@
                 <a class="nav-link note-link d-flex align-items-center justify-content-center px-3 text-body-color"
                     id="employee-tab" data-bs-toggle="pill" href="#employee-content" role="tab"
                     aria-controls="employee-content">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="me-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                        class="me-2">
                         <g fill="none">
                             <path
                                 d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
@@ -56,8 +57,7 @@
             </li>
 
             <li class="nav-item d-flex align-items-center ms-auto mt-2 mt-md-0 d-none" id="pegawai-buttons">
-                <button type="button" class="btn btn-primary px-4" data-bs-toggle="modal"
-                    data-bs-target="#create-level">
+                <button type="button" class="btn btn-primary px-4" data-bs-toggle="modal" data-bs-target="#create-level">
                     Tambah Tingkatan Kelas
                 </button>
             </li>
@@ -81,7 +81,6 @@
     @include('school.new.class.widgets.class-level.create-level')
 
     <x-delete-modal-component />
-
 @endsection
 @section('script')
     <script>
@@ -140,55 +139,55 @@
         });
     </script>
 
-<script>
-    $(document).ready(function() {
-        function resetActiveTab() {
-            $('.nav-link').removeClass('active');
-            $('.tab-pane').removeClass('active show');
-        }
-
-        function changeTab() {
-            var hash = window.location.hash;
-            resetActiveTab();
-            var tab = null;
-            switch (hash) {
-                case '#employee-content':
-                    tab = $('#employee-tab');
-                    break;
-                case '#teacher-content':
-                    tab = $('#teacher-tab');
-                    break;
-                default:
-                    tab = $('#teacher-tab');
-                    break;
+    <script>
+        $(document).ready(function() {
+            function resetActiveTab() {
+                $('.nav-link').removeClass('active');
+                $('.tab-pane').removeClass('active show');
             }
-            tab.addClass('active');
-            $(tab.attr('href')).addClass('active show');
-        }
 
-        function storeActiveTab() {
-            var activeTab = $('.nav-link.active').attr('href');
-            localStorage.setItem('activeTab', activeTab);
-        }
+            function changeTab() {
+                var hash = window.location.hash;
+                resetActiveTab();
+                var tab = null;
+                switch (hash) {
+                    case '#employee-content':
+                        tab = $('#employee-tab');
+                        break;
+                    case '#teacher-content':
+                        tab = $('#teacher-tab');
+                        break;
+                    default:
+                        tab = $('#teacher-tab');
+                        break;
+                }
+                tab.addClass('active');
+                $(tab.attr('href')).addClass('active show');
+            }
 
-        $(window).on('hashchange', function() {
-            changeTab();
-            storeActiveTab();
+            function storeActiveTab() {
+                var activeTab = $('.nav-link.active').attr('href');
+                localStorage.setItem('activeTab', activeTab);
+            }
+
+            $(window).on('hashchange', function() {
+                changeTab();
+                storeActiveTab();
+            });
+
+            changeTab(); // Initialize the correct tab on page load
+
+            $('.nav-link').on('shown.bs.tab', function() {
+                storeActiveTab();
+            });
+
+            var storedTab = localStorage.getItem('activeTab');
+            if (storedTab) {
+                window.location.hash = storedTab;
+            } else {
+                $('#teacher-tab').addClass('active');
+                $('#teacher-content').addClass('active show');
+            }
         });
-
-        changeTab(); // Initialize the correct tab on page load
-
-        $('.nav-link').on('shown.bs.tab', function() {
-            storeActiveTab();
-        });
-
-        var storedTab = localStorage.getItem('activeTab');
-        if (storedTab) {
-            window.location.hash = storedTab;
-        } else {
-            $('#teacher-tab').addClass('active');
-            $('#teacher-content').addClass('active show');
-        }
-    });
-</script>
+    </script>
 @endsection
--- resources/views/school/new/class/panes/class-tab.blade.php
+++ resources/views/school/new/class/panes/class-tab.blade.php
@@ -13,34 +13,37 @@
                 <div class="position-relative">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-center mb-1">
-                            <h2 class="fs-4 mb-0">{{ $classroom->name }}</h2>
-                            <span class="fs-2 ms-5">{{ $classroom->schoolYear->school_year }}</span>
-                            <div class="category-selector btn-group">
-                                <a class="nav-link category-dropdown label-group p-0" data-bs-toggle="dropdown"
-                                    href="#" role="button" aria-haspopup="true" aria-expanded="true">
-                                    <div class="category d-flex align-items-center">
-                                        <div class="category-business"></div>
-                                        <div class="category-social"></div>
-                                        <span class="more-options text-dark ms-2">
-                                            <i class="ti ti-dots-vertical fs-5"></i>
-                                        </span>
+                            <h4 class="mb-2"><b>{{ $classroom->name }}</b></h4>
+                            <div class="d-flex align-items-center">
+                                <span class="fs-3">{{ $classroom->schoolYear->school_year }}</span>
+                                <div class="category-selector btn-group">
+                                    <a class="nav-link category-dropdown label-group p-0" data-bs-toggle="dropdown"
+                                        href="#" role="button" aria-haspopup="true" aria-expanded="true">
+                                        <div class="category d-flex align-items-center">
+                                            <div class="category-business"></div>
+                                            <div class="category-social"></div>
+                                            <span class="more-options text-dark ms-2">
+                                                <i class="ti ti-dots-vertical fs-5"></i>
+                                            </span>
+                                        </div>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right category-menu"
+                                        data-popper-placement="bottom-end">
+                                        <button type="button" data-id="{{ $classroom->id }}" data-name="{{ $classroom->name }}" data-level="{{ $classroom->level_class_id }}" data-employee="{{ $classroom->employee_id }}" class="btn-update-classroom note-business badge-group-item badge-business dropdown-item position-relative category-business d-flex align-items-center btn-edit">
+                                            Edit
+                                        </button>
+                                        <button
+                                            class="note-business text-danger badge-group-item badge-business dropdown-item position-relative category-business d-flex align-items-center btn-delete-class"
+                                            data-id="{{ $classroom->id }}">
+                                            Hapus
+                                        </button>
                                     </div>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right category-menu"
-                                    data-popper-placement="bottom-end">
-                                    <button type="button" data-id="{{ $classroom->id }}" data-name="{{ $classroom->name }}" data-level="{{ $classroom->level_class_id }}" data-employee="{{ $classroom->employee_id }}" class="btn-update-classroom note-business badge-group-item badge-business dropdown-item position-relative category-business d-flex align-items-center btn-edit">
-                                        Edit
-                                    </button>
-                                    <button
-                                        class="note-business text-danger badge-group-item badge-business dropdown-item position-relative category-business d-flex align-items-center btn-delete-class"
-                                        data-id="{{ $classroom->id }}">
-                                        Hapus
-                                    </button>
                                 </div>
                             </div>
                         </div>
 
-                        <span class="fs-3">{{ $classroom->employee->user->name }}</span>
+
+                        <span class="fs-4">{{ $classroom->employee->user->name }}</span>
                         <div class="d-flex align-items-center mb-4 pt-3">
                             <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 16 16">
                                 <path fill="currentColor"
