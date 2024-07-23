6a3422f7a287700e65797c567b4fd2a9a78886dd
nesaathussholeha <nesaathussholeha@gmail.com>
2024-07-23 09:06:01 +0700

    update modal edit kelas

--- resources/views/school/new/class/widgets/class/update-class.blade.php
+++ resources/views/school/new/class/widgets/class/update-class.blade.php
@@ -1,6 +1,6 @@
 <!-- modal Edit -->
 <div class="modal fade" id="update-class" tabindex="-1" aria-labelledby="importPegawai" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="importPegawai">Edit Kelas</h5>
@@ -15,18 +15,28 @@
                         <label for="class-name" class="form-label">Nama Kelas</label>
                         <input type="text" class="form-control" id="name-edit" name="name" placeholder="Masukkan nama kelas">
                     </div>
-                    <div class="mb-3">
-                        <label for="class-teacher" class="form-label">Wali Kelas</label>
-                        <select class="form-control" id="employee-edit" name="employee_id">
-                            <option value="">Pilih wali kelas</option>
-                            @forelse ($teachers as $teacher)
-                                <option value="{{ $teacher->id }}">{{ $teacher->user->name }}</option>
-                            @empty
-                            @endforelse
-                        </select>
+                    <div class="row">
+                        <div class="mb-3 col-md-6">
+                            <label for="class-teacher" class="form-label">Wali Kelas</label>
+                            <select class="form-control" id="class-teacher" name="employee_id">
+                                <option value="">Pilih wali kelas</option>
+                                @forelse ($teachers as $teacher)
+                                    <option value="{{ $teacher->id }}">{{ $teacher->user->name }}</option>
+                                @empty
+                                @endforelse
+                            </select>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="class-level" class="form-label">Tingkatan Kelas</label>
+                            <select class="form-control" id="class-level" name="class_level_id">
+                                <option value="">Pilih tingkatan kelas</option>
+                                {{-- Populate options for class levels here --}}
+                            </select>
+                        </div>
                     </div>
+
                 </div>
-                
+
                 <div class="modal-footer">
                     <button type="button" class="btn mb-1 waves-effect waves-light btn-light" data-bs-dismiss="modal">Tutup</button>
                     <button type="submit" class="btn btn-rounded btn-primary" form="edit-class-form">Simpan</button>
