08379236bec98d2a785fa237ef198abf52712dd6
ArdianS18 <ardiansupriadi464@gmail.com>
2024-07-24 13:41:04 +0700

    name class

--- resources/views/school/new/class/detail.blade.php
+++ resources/views/school/new/class/detail.blade.php
@@ -7,7 +7,7 @@
                     <h4 class="fw-semibold mb-2 text-dark">Daftar Siswa</h4>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item" aria-current="page">X RPL 1</li>
+                            <li class="breadcrumb-item" aria-current="page">{{ $classroom->name }}</li>
                         </ol>
                     </nav>
                 </div>
