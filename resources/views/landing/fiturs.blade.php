149af4b3ac2925d7df22368316c0056d9080efa2
nesaathussholeha <nesaathussholeha@gmail.com>
2024-07-31 15:37:38 +0700

    Update fiturs.blade.php

--- resources/views/landing/fiturs.blade.php
+++ resources/views/landing/fiturs.blade.php
@@ -10,35 +10,40 @@
         <div class="features_inner" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="100">
             <!-- card -->
             <div class="feature_card">
-                <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}" alt="image">
+                <div style="text-align: start">
+                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}"  alt="image">
+                </div>
                 <div class="inner_text">
-                    <h3 class="pt-3">E - Learning</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus felis non odio convallis
+                    <h3 class="pt-3" style="text-align: start">E - Learning</h3>
+                    <p style="text-align: start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus felis non odio convallis
                         interdum. Cras id diam rhoncus,
                     </p>
                 </div>
             </div>
             <!-- card -->
             <div class="feature_card">
-                <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}" alt="image">
+                <div style="text-align: start">
+                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}"  alt="image">
+                </div>
                 <div class="inner_text">
-                    <h3 class="pt-3">Pixel perfect design</h3>
-                    <p>Lorem Ipsum is simply dummy text ofthe printing and type setting indus ideas orem Ipsum has
-                        beenthe.
+                    <h3 class="pt-3" style="text-align: start">E - Learning</h3>
+                    <p style="text-align: start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus felis non odio convallis
+                        interdum. Cras id diam rhoncus,
                     </p>
                 </div>
             </div>
             <!-- card -->
             <div class="feature_card">
-                <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}" alt="image">
+                <div style="text-align: start">
+                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}"  alt="image">
+                </div>
                 <div class="inner_text">
-                    <h3 class="pt-3">Lifetime free updates</h3>
-                    <p>Lorem Ipsum is simply dummy text ofthe printing and type setting indus ideas orem Ipsum has
-                        beenthe.
+                    <h3 class="pt-3" style="text-align: start">E - Learning</h3>
+                    <p style="text-align: start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus felis non odio convallis
+                        interdum. Cras id diam rhoncus,
                     </p>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
