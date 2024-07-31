56bf3dc5856bf47adaba350ffd46baf6dc707f3c
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-31 13:39:22 +0700

    layout landing page

---
+++ resources/views/layouts/landing/header.blade.php
@@ -0,0 +1,138 @@
+    <!-- Top Banner Start-->
+    <div class="top_home_wraper white_option">
+
+        <div class="container">
+            <!-- vertical line animation -->
+            <div class="anim_line dark_bg">
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+                <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
+            </div>
+        </div>
+
+        <!-- Header Start -->
+        <header class="fixed">
+            <!-- container start -->
+            <div class="container">
+                <!-- navigation bar -->
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="{{ asset('landing_assets/images/new/logo_white.png') }}" alt="image">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon">
+                            <!-- <i class="icofont-navigation-menu ico_menu"></i> -->
+                            <div class="toggle-wrap">
+                                <span class="toggle-bar"></span>
+                            </div>
+                        </span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about-us.html">About us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="testimonial.html">Testimonial</a>
+                            </li>
+                            <!-- secondery menu start -->
+                            <li class="nav-item has_dropdown">
+                                <a class="nav-link" href="#">Services</a>
+                                <span class="drp_btn"><i class="icofont-rounded-down"></i></span>
+                                <div class="sub_menu">
+                                    <ul>
+                                        <li><a href="service-list-1.html">Service List 1</a></li>
+                                        <li><a href="service-list-2.html">Service List 2</a></li>
+                                        <li><a href="service-detail.html">Service Details</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li class="nav-item has_dropdown">
+                                <a class="nav-link" href="#">Blog </a>
+                                <span class="drp_btn"><i class="icofont-rounded-down"></i></span>
+                                <div class="sub_menu">
+                                    <ul>
+                                        <li><a href="blog-list.html">Blog List</a></li>
+                                        <li><a href="blog-detail.html">Single Blog</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li class="nav-item has_dropdown">
+                                <a class="nav-link" href="#">Pages </a>
+                                <span class="drp_btn"><i class="icofont-rounded-down"></i></span>
+                                <div class="sub_menu">
+                                    <ul>
+                                        <li><a href="pricing.html">Pricing </a></li>
+                                        <li><a href="sign-in.html">Sign In </a></li>
+                                        <li><a href="sign-up.html">Sign Up </a></li>
+                                        <li><a href="error.html">Error 404 </a></li>
+                                        <li><a href="faq.html">Faq </a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- secondery menu end -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact-us.html">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link dark_btn" href="#getstarted">TRY IT FREE <i class="icofont-arrow-right"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <!-- navigation end -->
+            </div>
+            <!-- container end -->
+        </header>
+
+        <!-- Banner-Section-Start -->
+        <section class="banner_section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="banner_text">
+                            <div class="ban_inner_text" data-aos="fade-up" data-aos-duration="1500">
+                                <span><i class="icofont-rocket-alt-2"></i> Powerful, easier and faster</span>
+                                <h1>Build your network <br> <span>faster with apper</span></h1>
+                                <p>The World's simplest and fast platform that brings everything together</p>
+                            </div>
+
+                            <form action="https://kalanidhithemes.com/live-preview/landing-page/codely/all-demo/06-codely-landing-page-get-started-hero/submit" data-aos="fade-up" data-aos-duration="1500">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Enter your email">
+                                    <button class="btn btn_main">GET STARTED <i class="icofont-arrow-right"></i></button>
+                                </div>
+                            </form>
+
+                            <div class="offers" data-aos="fade-up" data-aos-duration="1500">
+                                <span><i class="icofont-check-circled"></i> Free trial 14 days</span>
+                                <span><i class="icofont-check-circled"></i> No credit card require</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-12">
+                        <div class="banner_image" data-aos="fade-up" data-aos-duration="1500">
+                            <img src="{{ asset('landing_assets/images/new/hero_image.png') }}" alt="image">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    </div>
+    <!-- Top Banner End-->
