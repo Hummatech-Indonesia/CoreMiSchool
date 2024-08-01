eda540a678127c97fd8e6a19a02336df6a0550c7
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-08-01 14:03:23 +0700

    layout header, banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,141 +66,6 @@
 
 @section('content')
 
-<!-- Top Banner Start-->
-<div class="top_home_wraper white_option">
-
-    <div class="container">
-        <!-- vertical line animation -->
-        <div class="anim_line dark_bg">
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-            <span><img src="{{ asset('landing_assets/images/anim_line.png') }}" alt="anim_line"></span>
-        </div>
-    </div>
-
-    <!-- Header Start -->
-    <header class="fixed">
-        <!-- container start -->
-        <div class="container">
-            <!-- navigation bar -->
-            <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand" href="index.html">
-                    <img src="{{ asset('assets/images/logo/logo-miscool.png') }}" alt="image">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon">
-                        <!-- <i class="icofont-navigation-menu ico_menu"></i> -->
-                        <div class="toggle-wrap">
-                            <span class="toggle-bar"></span>
-                        </div>
-                    </span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about-us.html">About us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="testimonial.html">Testimonial</a>
-                        </li>
-                        <!-- secondery menu start -->
-                        <li class="nav-item has_dropdown">
-                            <a class="nav-link" href="#">Services</a>
-                            <span class="drp_btn"><i class="icofont-rounded-down"></i></span>
-                            <div class="sub_menu">
-                                <ul>
-                                    <li><a href="service-list-1.html">Service List 1</a></li>
-                                    <li><a href="service-list-2.html">Service List 2</a></li>
-                                    <li><a href="service-detail.html">Service Details</a></li>
-                                </ul>
-                            </div>
-                        </li>
-
-                        <li class="nav-item has_dropdown">
-                            <a class="nav-link" href="#">Blog </a>
-                            <span class="drp_btn"><i class="icofont-rounded-down"></i></span>
-                            <div class="sub_menu">
-                                <ul>
-                                    <li><a href="blog-list.html">Blog List</a></li>
-                                    <li><a href="blog-detail.html">Single Blog</a></li>
-                                </ul>
-                            </div>
-                        </li>
-
-                        <li class="nav-item has_dropdown">
-                            <a class="nav-link" href="#">Pages </a>
-                            <span class="drp_btn"><i class="icofont-rounded-down"></i></span>
-                            <div class="sub_menu">
-                                <ul>
-                                    <li><a href="pricing.html">Pricing </a></li>
-                                    <li><a href="sign-in.html">Sign In </a></li>
-                                    <li><a href="sign-up.html">Sign Up </a></li>
-                                    <li><a href="error.html">Error 404 </a></li>
-                                    <li><a href="faq.html">Faq </a></li>
-                                </ul>
-                            </div>
-                        </li>
-
-                        <!-- secondery menu end -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact-us.html">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link dark_btn" href="#getstarted">TRY IT FREE <i class="icofont-arrow-right"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <!-- navigation end -->
-        </div>
-        <!-- container end -->
-    </header>
-
-    <!-- Banner-Section-Start -->
-    <section class="banner_section">
-        <div class="container">
-            <div class="d-flex justify-content-center">
-                <div class="">
-                    <div class="banner_text mb-5">
-                        <div class="ban_inner_text" data-aos="fade-up" data-aos-duration="1500">
-                            <span><i class="icofont-rocket-alt-2 text-primary"></i> Website Management Sekolah</span>
-                            <h1 class="text-primary"><span>Selamat Datang di</span><br>MISCHOOL</h1>
-                            <p style="padding: 0 170px;">Mischool merupakan website untuk mempermudahkan berbagai Sekolah-sekolah. Mischool adalah web management sekolah yang menyediakan Fitur Absensi, Pelanggaran,
-                                E-learning, Raport, Ujian, Buku Tamu</p>
-                        </div>
-
-                        <form action="https://kalanidhithemes.com/live-preview/landing-page/codely/all-demo/06-codely-landing-page-get-started-hero/submit" data-aos="fade-up" data-aos-duration="1500">
-                            <div>
-                                <button class="btn btn_main">Daftar Sekarang <i class="icofont-arrow-right"></i></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                {{-- <div class="col-lg-6 col-md-12">
-                            <div class="banner_image" data-aos="fade-up" data-aos-duration="1500">
-                                <img src="{{ asset('landing_assets/images/new/hero_image.png') }}" alt="image">
-            </div>
-        </div> --}}
-</div>
-
-</div>
-</section>
-
-</div>
-<!-- Top Banner End-->
-
-
 <!-- Unique features Start -->
 <section class="row_am unique_section">
     <div class="container">
@@ -215,7 +80,7 @@
             <!-- card -->
             <div class="feature_card">
                 <div style="text-align: start">
-                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}"  alt="image">
+                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}" alt="image">
                 </div>
                 <div class="inner_text">
                     <h3 class="pt-3" style="text-align: start">E - Learning</h3>
@@ -227,7 +92,7 @@
             <!-- card -->
             <div class="feature_card">
                 <div style="text-align: start">
-                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}"  alt="image">
+                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}" alt="image">
                 </div>
                 <div class="inner_text">
                     <h3 class="pt-3" style="text-align: start">E - Learning</h3>
@@ -239,7 +104,7 @@
             <!-- card -->
             <div class="feature_card">
                 <div style="text-align: start">
-                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}"  alt="image">
+                    <img src="{{ asset('landing_assets/images/new/Secure-data.svg') }}" alt="image">
                 </div>
                 <div class="inner_text">
                     <h3 class="pt-3" style="text-align: start">E - Learning</h3>
--- routes/role/landing.php
+++ routes/role/landing.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('about-us', function(){
     return view('landing.about-us');
 })->name('about-us');
+
+Route::get('news', function(){
+    return view('landing.news');
+})->name('news');
