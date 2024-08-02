f51398d453578fc72f86fbcdb6e445e1f8d25513
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-02 09:03:01 +0700

    update header layout 1

--- resources/views/layouts/landing/layouts/header.blade.php
+++ resources/views/layouts/landing/layouts/header.blade.php
@@ -57,7 +57,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="/landing">Beranda</a>
+                        <a class="nav-link" href="/">Beranda</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/about-us">Tentang Kami</a>
