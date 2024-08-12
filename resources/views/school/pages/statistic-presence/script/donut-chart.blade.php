00d0b122473a4985dd630e6e56b61e2986a6eb4c
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-08-12 15:51:53 +0700

    chart guru donut

--- resources/views/school/pages/statistic-presence/script/donut-chart.blade.php
+++ resources/views/school/pages/statistic-presence/script/donut-chart.blade.php
@@ -1,6 +1,6 @@
 {{-- statistik pegawai --}}
 
-<script>
+{{-- <script>
     var attendanceChartData = @json($attendanceEmployeeChart);
 
     var data1 = attendanceChartData.map(item => item.attendance_present);
@@ -16,7 +16,7 @@
         dataLabels: {
             enabled: false // Menyembunyikan persentase di dalam chart
         },
-        labels: ['Izin','Masuk','Sakit','Alpha'], // Menghapus tulisan series
+        labels: ['Izin','Masuk','Sakit','Alpha'],
         responsive: [{
             breakpoint: 480,
             options: {
@@ -28,6 +28,46 @@
                 }
             }
         }]
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart-employee"), options);
+    chart.render();
+</script> --}}
+{{-- Statistik Pegawai --}}
+
+<script>
+    var attendanceChartData = @json($attendanceEmployeeChart);
+
+    var dataPresent = attendanceChartData.map(item => item.attendance_present);
+    var dataPermit = attendanceChartData.map(item => item.attendance_permit);
+    var dataSick = attendanceChartData.map(item => item.attendance_sick);
+    var dataAlpha = attendanceChartData.map(item => item.attendance_alpha);
+
+    var options = {
+        series: [dataPermit.reduce((a, b) => a + b, 0), // Summing up values for the chart
+                 dataPresent.reduce((a, b) => a + b, 0), 
+                 dataSick.reduce((a, b) => a + b, 0), 
+                 dataAlpha.reduce((a, b) => a + b, 0)],
+        chart: {
+            type: 'donut',
+            height: 350
+        },
+        dataLabels: {
+            enabled: false // Menyembunyikan persentase di dalam chart
+        },
+        labels: ['Izin', 'Masuk', 'Sakit', 'Alpha'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }],
+        colors: ['#5D87FF', '#13DEB9', '#FFAE1F', '#FA896B']
     };
 
     var chart = new ApexCharts(document.querySelector("#chart-employee"), options);
