0e51a5414b2623a9d6128b66cbfadb247f6e9670
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-08-13 09:51:16 +0700

    Update detail-presence.blade.php

--- resources/views/school/pages/statistic-presence/detail-presence.blade.php
+++ resources/views/school/pages/statistic-presence/detail-presence.blade.php
@@ -23,7 +23,7 @@
                 <path fill="currentColor" d="M12 7q-.825 0-1.412-.587T10 5t.588-1.412T12 3t1.413.588T14 5t-.587 1.413T12 7m0 14q-.625 0-1.062-.437T10.5 19.5v-9q0-.625.438-1.062T12 9t1.063.438t.437 1.062v9q0 .625-.437 1.063T12 21" />
             </svg>
         </span>
-        <h5 class="ms-2 mb-1 fw-semibold">Statistik Absensi Pegawai</h5>
+        <h5 class="ms-2 mb-1 fw-semibold">Data Absensi Siswa</h5>
     </div>
 
     <div class="d-flex gap-2">
