112f9daf258343e08b67d538a9395c4a8a630edb
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-21 13:37:36 +0700

    Update import.blade.php

--- resources/views/school/pages/lesson-schedule/widgets/import.blade.php
+++ resources/views/school/pages/lesson-schedule/widgets/import.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="importPegawai">Import Jam Pelajaran</h5>
+                <h5 class="modal-title" id="importPegawai">Import Jadwal Pelajaran</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="" method="POST" enctype="multipart/form-data">
