92160e0d2f00b558e3382127a10f0add5a0eaf48
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-08-22 15:29:12 +0700

    slicing perbaikan siswa role staff

---
+++ resources/views/staff/pages/remedial-student-list/index.blade.php
@@ -0,0 +1,116 @@
+@extends('staff.layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-center gap-2 mb-2">
+    <span class="text-warning fw-semibold">50-79 Point</span>
+    <span class="text-danger fw-semibold">80-100</span>
+</div>
+<div class="card bg-light-info shadow-none position-relative overflow-hidden">
+    <div class="card-body px-4 py-3">
+        <div class="row align-items-center">
+            <div class="col-9">
+                <h4 class="fw-semibold mb-8">Perbaikan</h4>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a class="text-dark text-decoration-none"
+                                href="javascript:void(0)">Daftar siswa perbaikan untuk mengurangi point pelanggaran</a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="{{ asset('admin_assets/dist/images/breadcrumb/welcome.png') }}" alt=""
+                        class="img-fluid mb-n3" style="width: 170px; height: 120px; object-fit: cover;">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mb-3">
+    <form class="d-flex justify-content-between" action="">
+        <div class="d-flex gap-2">
+            <div class="position-relative">
+                <input type="text" name="name" class="form-control product-search ps-5" id="input-search" placeholder="Cari..." value="{{ old('name', request('name')) }}">
+                <i class="ti ti-search position-absolute top-50 start-0 translate-middle-y fs-6 text-dark ms-3"></i>
+            </div>
+            <div class="d-flex gap-2">
+                <select name="gender" class="form-select">
+                    <option value="all">Tampilkan semua</option>
+                    <option value="male" {{ request()->has('gender') == 'male' ? 'selected' : '' }}>Laki-laki</option>
+                    <option value="female" {{ request()->has('gender') == 'female' ? 'selected' : '' }}>Perempuan</option>
+                </select>
+                <div>
+                    <button type="submit" class="btn btn-primary btn-md">Filter</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex gap-2">
+            <button class="btn btn-success">
+                <svg xmlns="http://www.w3.org/2000/svg" class="mb-1 me-1" width="17" height="17" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M14 3v4a1 1 0 0 0 1 1h4"/><path d="M5 13V5a2 2 0 0 1 2-2h7l5 5v11a2 2 0 0 1-2 2h-5.5M2 19h7m-3-3l3 3l-3 3"/></g></svg>
+                Import Perbaikan
+            </button>
+            <button class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" class="mb-1 me-1" width="17" height="17" viewBox="0 0 24 24"><path fill="currentColor" d="M4 22q-.825 0-1.412-.587T2 20V8q0-.825.588-1.412T4 6h4V4q0-.825.588-1.412T10 2h4q.825 0 1.413.588T16 4v2h4q.825 0 1.413.588T22 8v12q0 .825-.587 1.413T20 22zm0-2h16V8H4zm6-14h4V4h-4zM4 20V8zm7-5v3h2v-3h3v-2h-3v-3h-2v3H8v2z"/></svg>
+                Tambah Perbaikan
+            </button>
+        </div>
+    </form>
+</div>
+
+<div class="table-responsive rounded-2 mb-4">
+    <table class="table border text-nowrap customize-table mb-0 align-middle">
+        <thead class="text-dark fs-4">
+            <tr class="">
+                <th>No</th>
+                <th>Nama</th>
+                <th>Durasi Pengerjaan</th>
+                <th>Perbaikan</th>
+                <th>Status</th>
+                <th>Point</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse (range(1,5) as $student)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>
+                        <div class="d-flex align-items-center">
+                            <img src="{{ asset('admin_assets/dist/images/profile/user-10.jpg') }}" class="rounded-circle me-2 user-profile" style="object-fit: cover" width="40" height="40" alt="" />
+                            <div class="ms-3">
+                                <h6 class="fs-4 fw-semibold mb-0 text-start">Ahmad Lukman</h6>
+                                <span class="fw-normal">123456789</span>
+                            </div>
+                        </div>
+                    </td>
+                    <td>10 Mei 2024 - 10 Mei 2024</td>
+                    <td>Menyapu halaman sekolah</td>
+                    <td>
+                        <span class="badge bg-light-success text-success">Sudah Dikerjakan</span>
+                    </td>
+                    <td>
+                        <span class="badge bg-light-primary text-primary">-80 Point</span>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-primary py-1 px-4">Detail</a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="7" class="text-center align-middle">
+                        <div class="d-flex flex-column justify-content-center align-items-center">
+                            <img src="{{ asset('admin_assets/dist/images/empty/no-data.png') }}" alt="" width="300px">
+                            <p class="fs-5 text-dark text-center mt-2">
+                                Siswa belum ditambahkan
+                            </p>
+                        </div>
+                    </td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+</div>
+@endsection
