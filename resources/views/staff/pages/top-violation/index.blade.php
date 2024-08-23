23853d3324bd040c9a3955137475087de70252c4
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-23 09:02:25 +0700

    Update index.blade.php

--- resources/views/staff/pages/top-violation/index.blade.php
+++ resources/views/staff/pages/top-violation/index.blade.php
@@ -33,23 +33,55 @@
 @endsection
 
 @section('content')
-    <div class="card bg-light-info shadow-none position-relative overflow-hidden">
-        <div class="card-body px-4 py-3">
-            <div class="row align-items-center">
-                <div class="col-9">
-                    <h4 class="fw-semibold mb-8">Pelanggaran Terbanyak</h4>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a class="text-dark text-decoration-none"
-                                    href="javascript:void(0)">Daftar siswa perbaikan untuk mengurangi point pelanggaran</a>
-                            </li>
-                        </ol>
-                    </nav>
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card bg-primary shadow-none position-relative overflow-hidden">
+                <div class="card-body px-4 py-3">
+                    <div class="row align-items-center">
+                        <div class="col-9">
+                            <b class="text-white fs-2">Daftar Point Siswa</b>
+                            <h4 class="fw-semibold text-white fs-5 mt-1">30 Siswa
+                                Melanggar</h4>
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a class="text-white fs-2" href="javascript:void(0)">Daftar
+                                            daftar siswa yang melanggar akan mendapatkan point sesuai apa yang dilanggar</a>
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                        <div class="col-3">
+                            <div class="text-center mb-n5">
+                                <img src="{{ asset('admin_assets/dist/images/breadcrumb/pagar.png') }}" width="130px"
+                                    alt="" class="img-fluid mb-3">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-3">
-                    <div class="text-center">
-                        <img src="{{ asset('admin_assets/dist/images/breadcrumb/welcome.png') }}" alt=""
-                            class="img-fluid mb-n3" style="width: 170px; height: 120px; object-fit: cover;">
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card shadow-none position-relative overflow-hidden"
+                style="background: linear-gradient(to bottom, #FFE252, #ffc107);">
+                <div class="card-body px-4 py-3">
+                    <div class="row align-items-center">
+                        <div class="col-12">
+                            <div class="d-flex justify-content-between mb-2">
+                                <h5 class="fw-semibold text-white mb-8">Statistik Absensi</h5>
+                                <span class="mb-1 badge bg-white p-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="text-warning" width="15"
+                                        height="15" viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                            d="M12 7q-.825 0-1.412-.587T10 5t.588-1.412T12 3t1.413.588T14 5t-.587 1.413T12 7m0 14q-.625 0-1.062-.437T10.5 19.5v-9q0-.625.438-1.062T12 9t1.063.438t.437 1.062v9q0 .625-.437 1.063T12 21" />
+                                    </svg>
+                                </span>
+                            </div>
+                            <nav aria-label="breadcrumb">
+                                <span
+                                    class="badge fw-semibold fs-6 text-warning bg-white p-2">23
+                                    Point</span>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
