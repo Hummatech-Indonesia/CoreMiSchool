afc43a0861773b698a5bee89f0333991d59133b6
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-23 09:10:52 +0700

    Update sidebar.blade.php

--- resources/views/staff/layouts/sidebar.blade.php
+++ resources/views/staff/layouts/sidebar.blade.php
@@ -74,7 +74,7 @@
                     </a>
                 </li>
 
-                {{-- <li class="sidebar-item ">
+                <li class="sidebar-item ">
                     <a class="sidebar-link" href="/employee/violation-student-list" aria-expanded="false">
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
@@ -88,17 +88,19 @@
                         </span>
                         <span class="hide-menu">Daftar Pelanggaran</span>
                     </a>
-                </li> --}}
+                </li>
 
                 <li class="sidebar-item ">
                     <a class="sidebar-link" href="javascript:void(0)" aria-expanded="false">
                         <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
                                 <g fill="none">
-                                    <path
-                                        d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                                    <rect width="38" height="26" x="5" y="16" stroke="currentColor"
+                                        stroke-linejoin="round" stroke-width="4" rx="3" />
                                     <path fill="currentColor"
-                                        d="m13.299 3.148l8.634 14.954a1.5 1.5 0 0 1-1.299 2.25H3.366a1.5 1.5 0 0 1-1.299-2.25l8.634-14.954c.577-1 2.02-1 2.598 0M12 4.898L4.232 18.352h15.536zM12 15a1 1 0 1 1 0 2a1 1 0 0 1 0-2m0-7a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0V9a1 1 0 0 1 1-1" />
+                                        d="M19 8h10V4H19zm11 1v7h4V9zm-12 7V9h-4v7zm11-8a1 1 0 0 1 1 1h4a5 5 0 0 0-5-5zM19 4a5 5 0 0 0-5 5h4a1 1 0 0 1 1-1z" />
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="4" d="M18 29h12m-6-6v12" />
                                 </g>
                             </svg>
                         </span>
