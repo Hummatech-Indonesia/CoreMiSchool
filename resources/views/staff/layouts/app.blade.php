dcb21a5a77b591039e4fed6d0bd31e735518aeb5
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-23 10:58:19 +0700

    Update app.blade.php

--- resources/views/staff/layouts/app.blade.php
+++ resources/views/staff/layouts/app.blade.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 01:54:29 GMT -->
-
 <head>
     <!--  Title -->
     <title>{{ env('APP_NAME') }}</title>
@@ -11,16 +9,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="handheldfriendly" content="true" />
     <meta name="MobileOptimized" content="width" />
-    <meta name="description" content="Mordenize" />
+    <meta name="description" content="Modernize" />
     <meta name="author" content="" />
-    <meta name="keywords" content="Mordenize" />
+    <meta name="keywords" content="Modernize" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!--  Favicon -->
     <link rel="shortcut icon" type="image/png" href="{{ asset('mobilelogo.png') }}" />
-    <!-- Owl Carousel  -->
-    <link rel="stylesheet" href="{{ asset('admin_assets/dist/libs/owl.carousel/dist/owl.carousel.min.js') }}">
 
-    <!-- Core Css -->
+    <!-- Core CSS -->
     <link id="themeColors" rel="stylesheet" href="{{ asset('admin_assets/dist/css/style.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('admin_assets/dist/libs/select2/dist/css/select2.min.css') }}">
     @yield('style')
@@ -31,17 +27,13 @@
     <div class="preloader">
         <img src="{{ asset('assets/images/logo/logo-M.png') }}" style="width:100px" alt="loader" class="lds-ripple" />
     </div>
-    <!-- Preloader -->
-    <div class="preloader">
-        <img src="{{ asset('assets/images/logo/logo-M.png') }}" style="width:100px" alt="loader" class="lds-ripple" />
-    </div>
+
     <!--  Body Wrapper -->
     <div class="page-wrapper" id="main-wrapper" data-theme="blue_theme" data-layout="vertical" data-sidebartype="full"
         data-sidebar-position="fixed" data-header-position="fixed">
         <!-- Sidebar Start -->
         @include('staff.layouts.sidebar')
         <!--  Sidebar End -->
-        <!-- SignIn modal content -->
 
         <!--  Main wrapper -->
         <div class="body-wrapper">
@@ -49,40 +41,21 @@
             @include('staff.layouts.header')
             <!--  Header End -->
             <div class="px-4" style="padding-top: calc(70px + 15px);">
-                <!--  Owl carousel -->
                 @yield('content')
             </div>
         </div>
     </div>
-    <!--  Customizer -->
 
-    <!--  Customizer -->
     <!--  Import Js Files -->
-    @yield('script')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <script src="{{ asset('admin_assets/dist/libs/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/libs/simplebar/dist/simplebar.min.js') }}"></script>
     <script src="{{ asset('admin_assets/dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <!--  core files -->
+    <script src="{{ asset('admin_assets/dist/libs/select2/dist/js/select2.min.js') }}"></script>
     <script src="{{ asset('admin_assets/dist/js/app.min.js') }}"></script>
     <script src="{{ asset('admin_assets/dist/js/app.init.js') }}"></script>
     <script src="{{ asset('admin_assets/dist/js/app-style-switcher.js') }}"></script>
     <script src="{{ asset('admin_assets/dist/js/sidebarmenu.js') }}"></script>
     <script src="{{ asset('admin_assets/dist/js/custom.js') }}"></script>
-    <!--  current page js files -->
-    <script src="{{ asset('admin_assets/dist/libs/owl.carousel/dist/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/js/dashboard.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/libs/jquery.repeater/jquery.repeater.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/js/plugins/repeater-init.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="{{ asset('admin_assets/dist/libs/select2/dist/js/select2.full.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/libs/select2/dist/js/select2.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/js/forms/select2.init.js') }}"></script>
-
+    @yield('script')
 </body>
-
-<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 01:55:21 GMT -->
 
 </html>
