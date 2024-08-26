971523d82c9de55e5fb5d598897edff1266e9a35
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-26 11:11:14 +0700

    update no data kehadiran guru

--- resources/views/school/pages/attendance/teacher/index.blade.php
+++ resources/views/school/pages/attendance/teacher/index.blade.php
@@ -81,7 +81,7 @@
                         <div class="d-flex flex-column justify-content-center align-items-center">
                             <img src="{{ asset('admin_assets/dist/images/empty/no-data.png') }}" alt="" width="300px">
                             <p class="fs-5 text-dark text-center mt-2">
-                                Siswa belum ditambahkan
+                                Guru belum ditambahkan
                             </p>
                         </div>
                     </td>
