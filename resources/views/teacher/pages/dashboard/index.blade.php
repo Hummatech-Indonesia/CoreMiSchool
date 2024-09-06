1447fc710cba57c60b432f7e0207666f35c51ea0
nesaathussholeha <nesaathussholeha@gmail.com>
2024-09-06 09:35:38 +0700

    update dashboard guru - blm fix

--- resources/views/teacher/pages/dashboard/index.blade.php
+++ resources/views/teacher/pages/dashboard/index.blade.php
@@ -163,11 +163,11 @@
                                         <td>10 Agustus 2024</td>
                                         <td>07.00</td>
                                         <td>
-                                            <span class="mb-1 badge font-medium bg-light-secondary text-secondary">Masuk</span>
+                                            <span
+                                                class="mb-1 badge font-medium bg-light-secondary text-secondary">Masuk</span>
                                         </td>
                                     </tr>
                                 @empty
-                                    
                                 @endforelse
                             </tbody>
                         </table>
@@ -175,12 +175,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-4">
-            <div class="card">
+        <div class="col-lg-4 d-flex">
+            <div class="card w-100 h-100 overflow-hidden border">
                 <div class="card-body">
-                    <h4>Statistik Absensi Harisn</h4>
-                    <h6 class="mt-1">Tahun ini</h6>
-                    <div id="chart-teacher" class="mt-5"></div>
+                    <div class="row align-items-center">
+                        <h5 class="card-title fw-semibold">Statistik Absensi Siswa</h5>
+                        <h6 class="mb-3">Hari ini</h6>
+                        <div id="chart-teacher" class="d-flex justify-content-center"></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -196,26 +198,32 @@
                     <div class="card w-100">
                         <div class="card-header bg-primary" style="border-radius: 0.50rem;">
                             <h4 class="mb-0 text-white card-title">
-                                X RPL 1 - Bahasa Indonesia {{-- {{ $journal->classroom->name }} - {{ $journal->teacherSubject->subject->name }} --}}
+                                X RPL 1 - Bahasa Indonesia
                             </h4>
                             <div class="position-absolute top-0 end-0" style="padding: 0px; position: relative;">
-                                <img src="{{ asset('assets/images/background/arrow-leftwarning.png') }}" alt="Description" class="img-fluid" style="max-width: 210px; height: auto; position: relative;">
-                                <span class="d-flex align-items-center ms-5 fs-4" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; font-weight: bold;width: 100%;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="me-2" width="20" height="20" viewBox="0 0 24 24">
-                                        <path fill="currentColor" d="M12 12h5v5h-5zm7-9h-1V1h-2v2H8V1H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 2v2H5V5zM5 19V9h14v10z" />
+                                <img src="{{ asset('assets/images/background/arrow-leftwarning.png') }}" alt="Description"
+                                    class="img-fluid" style="max-width: 210px; height: auto; position: relative;">
+                                <span class="d-flex align-items-center ms-5 fs-4"
+                                    style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; font-weight: bold;width: 100%;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="me-2" width="20" height="20"
+                                        viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                            d="M12 12h5v5h-5zm7-9h-1V1h-2v2H8V1H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 2v2H5V5zM5 19V9h14v10z" />
                                     </svg>
                                     10 Agustus 2024
                                 </span>
                             </div>
                         </div>
-            
+
                         <div class="card-body">
                             <div class="row pb-2" style="border-bottom: 1px solid #c0c0c0">
                                 <div class="col-lg-8" style="border-right: 1px solid #c0c0c0;">
                                     <div class="pe-3">
                                         <h5 class="card-title mb-4">Deskripsi:</h5>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vehicula lacus massa, a finibus urna hendrerit fringilla.
-                                            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vehicula lacus
+                                            massa, a finibus urna hendrerit fringilla.
+                                            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                                            himenaeos.
                                             lorem ipsum....</p>
                                     </div>
                                 </div>
@@ -225,19 +233,22 @@
                                         <div class="row px-5">
                                             <div class="col-lg-4">
                                                 <div class="text-center">
-                                                    <span class="badge bg-light-primary text-primary fs-7 fw-semibold mb-1 py-2">10</span>
+                                                    <span
+                                                        class="badge bg-light-primary text-primary fs-7 fw-semibold mb-1 py-2">10</span>
                                                     <p>Izin</p>
                                                 </div>
                                             </div>
                                             <div class="col-lg-4">
                                                 <div class="text-center">
-                                                    <span class="badge bg-light-warning text-warning fs-7 fw-semibold mb-1 py-2">10</span>
+                                                    <span
+                                                        class="badge bg-light-warning text-warning fs-7 fw-semibold mb-1 py-2">10</span>
                                                     <p>Sakit</p>
                                                 </div>
                                             </div>
                                             <div class="col-lg-4">
                                                 <div class="text-center">
-                                                    <span class="badge bg-light-danger text-danger fs-7 fw-semibold mb-1 py-2">10</span>
+                                                    <span
+                                                        class="badge bg-light-danger text-danger fs-7 fw-semibold mb-1 py-2">10</span>
                                                     <p>Alfa</p>
                                                 </div>
                                             </div>
@@ -245,14 +256,18 @@
                                     </div>
                                 </div>
                             </div>
-            
+
                             <div>
                                 <a href="#" class="btn btn-primary mt-3">
                                     Lihat Detail Jurnal
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mb-1" viewBox="0 0 24 24"><path fill="currentColor" d="M17.92 11.62a1 1 0 0 0-.21-.33l-5-5a1 1 0 0 0-1.42 1.42l3.3 3.29H7a1 1 0 0 0 0 2h7.59l-3.3 3.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l5-5a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mb-1"
+                                        viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                            d="M17.92 11.62a1 1 0 0 0-.21-.33l-5-5a1 1 0 0 0-1.42 1.42l3.3 3.29H7a1 1 0 0 0 0 2h7.59l-3.3 3.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l5-5a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76" />
+                                    </svg>
                                 </a>
                             </div>
-            
+
                         </div>
                     </div>
                 </div>
@@ -261,12 +276,45 @@
         </div>
     </div>
 
-    <button class="btn mb-5 waves-effect waves-light btn-outline-info w-100" type="button">Lihat Selengkapnya</button>
+    <button class="btn mb-5 waves-effect waves-light btn-outline-info w-100" type="button">Lihat Selengkapnya
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16">
+            <path fill="currentColor"
+                d="M8.22 2.97a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.042-.018a.75.75 0 0 1-.018-1.042l2.97-2.97H3.75a.75.75 0 0 1 0-1.5h7.44L8.22 4.03a.75.75 0 0 1 0-1.06" />
+        </svg>
+    </button>
 
     <div class="row">
         <div class="col-lg-12 col-md-12">
-            <div class="card">
+            <div class="card border">
                 <div class="card-body">
+                    <h4 class="mb-4">Daftar Murid Dikelas Anda</h4>
+                    <div class="row mb-4">
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Cari Sekolah..." id="searchStudent">
+                        </div>
+
+                        <div class="col-md-2">
+                            <select class="form-select" id="filterGender">
+                                <option value="">10 - 30 poin</option>
+                                <option value="male">30 - 60 poin</option>
+                                <option value="female">60 - 100 poin</option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-2">
+                            <select class="form-select" id="filterAttendance">
+                                <option value="">Tampilkan Semua</option>
+                                <option value="">Terbaru</option>
+                                <option value="">Terlama</option>
+
+                            </select>
+                        </div>
+
+                        <div class="col-md-2">
+                            <button class="btn btn-primary">Filter</button>
+                        </div>
+                    </div>
+
                     <div class="table-responsive rounded-2 mt-3">
                         <table class="table border text-nowrap customize-table mb-0 align-middle">
                             <thead>
@@ -286,11 +334,11 @@
                                         <td>jovita@gmail.com</td>
                                         <td>3o point</td>
                                         <td>
-                                            <button type="button" class="btn font-medium btn-primary text-white">Detail</button>
+                                            <button type="button"
+                                                class="btn font-medium btn-primary text-white">Detail</button>
                                         </td>
                                     </tr>
                                 @empty
-                                    
                                 @endforelse
                             </tbody>
                         </table>
