7a99c9655623667bde0731cfda31672836256937
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-09-06 10:13:35 +0700

    slicing guest book role school

---
+++ resources/views/school/pages/guest-book/widgets/detail.blade.php
@@ -0,0 +1,74 @@
+<!-- modal detail -->
+<div class="modal fade" id="modal-detail" tabindex="-1" aria-labelledby="importPegawai" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content modal-lg">
+            <div class="modal-header">
+                <h5 class="modal-title" id="importPegawai"><b>Detail Pengunjung</b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="row mt-1">
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex" style="margin-bottom: 0.5rem;">
+                            <h6 style="margin-bottom: 0;">Nama:</h6>
+                            <p class="ms-2" style="margin-bottom: 0;" id="name-detail">Ahmad Lukman Hakim</p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex" style="margin-bottom: 0.5rem;">
+                            <h6 style="margin-bottom: 0;">Email:</h6>
+                            <p class="ms-2" style="margin-bottom: 0;" id="email-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex" style="margin-bottom: 0.5rem;">
+                            <h6 style="margin-bottom: 0;">Nomor Telepon:</h6>
+                            <p class="ms-2" style="margin-bottom: 0;" id="nisn-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex" style="margin-bottom: 0.5rem;">
+                            <h6 style="margin-bottom: 0;">Status:</h6>
+                            <p class="ms-2" style="margin-bottom: 0;" id="classroom-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex" style="margin-bottom: 0.5rem;">
+                            <h6 style="margin-bottom: 0;">Jabatan:</h6>
+                            <p class="ms-2" style="margin-bottom: 0;" id="gender-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex" style="margin-bottom: 0.5rem;">
+                            <h6 style="margin-bottom: 0;">Berasal Dari:</h6>
+                            <p class="ms-2" style="margin-bottom: 0;" id="religion-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="d-flex text-start">
+                            <h6 style="margin-bottom: 0;">Tanggal Berkunjung:</h6>
+                            <p class="ms-2 text-muted text-break" style="margin-bottom: 0;" id="birth-date-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-12 col-md-12">
+                        <div class="d-flex text-start">
+                            <h6 style="margin-bottom: 0;">Keperluan:</h6>
+                            <p class="ms-2 text-muted text-break" style="margin-bottom: 0;" id="address-detail"></p>
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn mb-1 waves-effect waves-light btn-danger" data-bs-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
