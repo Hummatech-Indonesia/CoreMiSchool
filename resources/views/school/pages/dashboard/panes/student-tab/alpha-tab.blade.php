a790846092d6b8985b928e7f971a2417dc242fa1
nesaathussholeha <nesaathussholeha@gmail.com>
2024-09-09 11:35:05 +0700

    no data dashboard sekolah

--- resources/views/school/pages/dashboard/panes/student-tab/alpha-tab.blade.php
+++ resources/views/school/pages/dashboard/panes/student-tab/alpha-tab.blade.php
@@ -10,7 +10,8 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($alpha as $item)
+
+            @forelse ($alpha as $item)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td>
@@ -24,7 +25,17 @@
                         <span class="badge bg-light-danger text-danger fw-semibold fs-2">Alpha</span>
                     </td>
                 </tr>
-            @endforeach
+            @empty
+                <tr>
+                    <td colspan="7" class="text-center align-middle">
+                        <div class="d-flex flex-column justify-content-center align-items-center">
+                            <img src="{{ asset('admin_assets/dist/images/empty/no-data.png') }}" alt=""
+                                width="300px">
+                            <p class="fs-5 text-dark text-center mt-2">Belum ada data</p>
+                        </div>
+                    </td>
+                </tr>
+            @endforelse
         </tbody>
     </table>
 </div>
