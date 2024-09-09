e5a06b78fde51e7131b1c6d50422520ed00b3af0
ArdianS18 <ardiansupriadi464@gmail.com>
2024-09-09 15:39:36 +0700

    Update EmployeeJournalCommand.php

--- app/Console/Commands/EmployeeJournalCommand.php
+++ app/Console/Commands/EmployeeJournalCommand.php
@@ -35,21 +35,22 @@ class EmployeeJournalCommand extends Command
 
         if($day != 'Sunday')
         {
-            $employee_ids = EmployeeJournal::whereDate('created_at', $dateNow)->pluck('employee_id')->array();
+            $employee_ids = EmployeeJournal::whereDate('created_at', $dateNow)->pluck('employee_id')->toArray();
             $employee_notYet = Employee::query()
                     ->whereRelation('user.roles', 'name', RoleEnum::STAFF->value)
                     ->whereNotin('id', $employee_ids)
                     ->get();
 
-            foreach ($employee_ids as $employee) {
+            foreach ($employee_notYet as $employee) {
                 EmployeeJournal::create([
-                    'emplpyee_id' => $employee,
+                    'employee_id' => $employee->id,
                     'title' => 'Kosong',
                     'description' => 'Kosong',
                     'status' => StatusEnum::NOT_COMPLETED
                 ]);
             }
-
         }
+
+        $this->info('Command berhasil dijalankan!');
     }
 }
