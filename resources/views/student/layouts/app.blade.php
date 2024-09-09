012ad45398aacf1d5fd11755dd7e6ccfe93fb177
nesaathussholeha <nesaathussholeha@gmail.com>
2024-09-09 09:49:09 +0700

    update app student

--- resources/views/student/layouts/app.blade.php
+++ resources/views/student/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Core Css -->
     <link id="themeColors" rel="stylesheet" href="{{ asset('admin_assets/dist/css/style.min.css') }}" />
-    
+
     @yield('style')
 </head>
 
@@ -71,8 +71,8 @@
     <script src="{{ asset('admin_assets/dist/js/custom.js') }}"></script>
     <!--  current page js files -->
     <script src="{{ asset('admin_assets/dist/libs/owl.carousel/dist/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/js/dashboard.js') }}"></script>
-    <script src="{{ asset('admin_assets/dist/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    {{-- <script src="{{ asset('admin_assets/dist/js/dashboard.js') }}"></script> --}}
+    {{-- <script src="{{ asset('admin_assets/dist/libs/apexcharts/dist/apexcharts.min.js') }}"></script> --}}
 
 
 </body>
