f1d3b705d0fc216a08dc09790786dd6fbd5430b0
nesaathussholeha <nesaathussholeha@gmail.com>
2024-09-10 13:09:43 +0700

    update dashboard guru

---
+++ resources/views/teacher/pages/dashboard/panes/absence-history.blade.php
@@ -0,0 +1,55 @@
+<div class="col-lg-8">
+    <div class="card">
+        <div class="card-body">
+            <h4>Riwayat Absensi</h4>
+            <div class="table-responsive rounded-2 mt-3">
+                <table class="table border text-nowrap customize-table mb-0 align-middle">
+                    <thead>
+                        <tr>
+                            <th class="text-white" style="background-color: #5D87FF;">Hari</th>
+                            <th class="text-white" style="background-color: #5D87FF;">Tanggal</th>
+                            <th class="text-white" style="background-color: #5D87FF;">Masuk</th>
+                            <th class="text-white" style="background-color: #5D87FF;">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($attendances->take(5) as $attendance)
+                            <tr>
+                                <td>{{ $attendance->created_at->translatedFormat('l') }}</td>
+                                <td>{{ $attendance->created_at->format('d F Y') }}</td>
+                                <td>{{ $attendance->checkin }}</td>
+                                <td>
+                                    <span
+                                        class="mb-1 badge font-medium {{ $attendance->status->color() }}">{{ $attendance->status->label() }}</span>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4" class="text-center align-middle">
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <img src="{{ asset('admin_assets/dist/images/empty/no-data.png') }}"
+                                            alt="" width="300px">
+                                        <p class="fs-5 text-dark text-center mt-2">
+                                            Belum ada data
+                                        </p>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 d-flex">
+    <div class="card w-100 h-100 overflow-hidden border">
+        <div class="card-body">
+            <div class="row align-items-center">
+                <h5 class="card-title fw-semibold">Statistik Absensi Guru</h5>
+                <h6 class="mb-3">Hari ini</h6>
+                <div id="chart-teacher" class="d-flex justify-content-center"></div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/teacher/pages/dashboard/panes/profile.blade.php
@@ -0,0 +1,50 @@
+<div class="col-lg-6">
+    <div class="card">
+        <div class="card-body">
+            <h4><b>Profil guru</b></h4>
+            <div class="row align-items-center">
+                <div class="col-lg-3">
+                    <img src="{{ asset('assets/images/default-user.jpeg') }}" width="120px" alt=""
+                        class="img-fluid">
+                </div>
+                <div class="col-lg-8 ms-1">
+                    <h3><b>{{ auth()->user()->name }}</b></h3>
+                    <h5>Tahun Ajaran {{ $schoolYear->school_year }}</h5>
+                    <div class="pt-2">
+                        @forelse ($teacherSubjects as $teacherSubject)
+                            <div class="m-1 badge bg-light-primary text-primary">
+                                {{ $teacherSubject->subject->name }}</div>
+                        @empty
+                            <div class="badge bg-light-warning text-warning">Anda tidak megajar pelajaran apapun
+                            </div>
+                        @endforelse
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-lg-6">
+    <div class="card">
+        <div class="card-body">
+            <div class="row align-items-center">
+                @if ($classroom)
+                    <div class="col-lg-8">
+                        <h5><b>Wali Kelas Dari</b></h5>
+                        <h3 class="my-4"><b>{{ $classroom->name }}</b></h3>
+                        <div class="badge bg-light-primary text-primary fs-5">
+                            {{ $classroom->classroomStudents->count() }} Total Siswa</div>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="{{ asset('assets/images/Topi.png') }}">
+                    </div>
+                @else
+                    <div class="col-lg-12">
+                        <img src="{{ asset('assets/images/Topi.png') }}">
+                        <h4>Anda tidak menjadi wali kelas manapun</h4>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
