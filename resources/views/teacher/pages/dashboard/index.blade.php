df13b0d619614c6319bc35d30bbabb488f207566
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-09-10 09:39:27 +0700

    Update index.blade.php

--- resources/views/teacher/pages/dashboard/index.blade.php
+++ resources/views/teacher/pages/dashboard/index.blade.php
@@ -370,6 +370,16 @@
                                         </td>
                                     </tr>
                                 @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center align-middle">
+                                            <div class="d-flex flex-column justify-content-center align-items-center">
+                                                <img src="{{ asset('admin_assets/dist/images/empty/no-data.png') }}" alt="" width="300px">
+                                                <p class="fs-5 text-dark text-center mt-2">
+                                                    Kelas kosong
+                                                </p>
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @endforelse
                             </tbody>
                         </table>
