86555e6b962fcdabdfc2a74d7b1507e6c668e88e
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-09-23 09:52:12 +0700

    Update api.php

--- config/api.php
+++ config/api.php
@@ -2,5 +2,5 @@
 
 return [
     'check_rfid' => 'https://mischool.mijurnal.com/api/rfid-check',
-    'api_login' => 'http://mischool.mijurnal.com/api/login'
+    'api_login' => 'https://mischool.mijurnal.com/api/login'
 ];
