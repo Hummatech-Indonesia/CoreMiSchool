acb0dce511b61897a9205e67a7447889b6d3064c
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-09-23 10:54:18 +0700

    Update class.blade.php

--- resources/views/staff/pages/top-violation/panes/class.blade.php
+++ resources/views/staff/pages/top-violation/panes/class.blade.php
@@ -44,7 +44,11 @@
                             <h5><strong>Jumlah Point Keseluruhan :</strong></h5>
                         </div>
                         <div>
-                            <span class="mb-1 badge font-medium bg-light-danger text-danger"> {{ $classroom->classroomStudents->sum(function($student){ return $student->student->sum('point'); }) }} Point </span>
+                            <span class="mb-1 badge font-medium bg-light-danger text-danger">
+                                {{ $classroom->classroomStudents->sum(function ($student) {
+                                    return $student->student->studentViolations->sum('point');
+                                }) }}
+                                Point </span>
                         </div>
                     </div>
                     <div class="card mt-3 mb-0">
@@ -57,12 +61,16 @@
                                         stroke-linejoin="round" stroke-width="1.5"
                                         d="M17.928 19.634h2.138a1.165 1.165 0 0 0 1.116-1.555a6.851 6.851 0 0 0-6.117-3.95m0-2.759a3.664 3.664 0 0 0 3.665-3.664a3.664 3.664 0 0 0-3.665-3.674m-1.04 16.795a1.908 1.908 0 0 0 1.537-3.035a8.026 8.026 0 0 0-6.222-3.196a8.026 8.026 0 0 0-6.222 3.197a1.909 1.909 0 0 0 1.536 3.034zM9.34 11.485a4.16 4.16 0 0 0 4.15-4.161a4.151 4.151 0 0 0-8.302 0a4.16 4.16 0 0 0 4.151 4.16" />
                                 </svg>
-                                <h5>{{ $classroom->classroomStudents->sum(function($student) { return $student->studentViolations->count(); }) }} Siswa</h5>
+                                <h5>{{ $classroom->classroomStudents->reduce(function ($carry, $student) {
+                                    return $carry + $student->studentViolations->count();
+                                }, 0) }}
+                                    Siswa</h5>
                             </div>
                         </div>
                     </div>
                     <div class="mt-3">
-                        <a href="{{ route('employee.violation.class-point.detail', ['classroom' => $classroom->id]) }}" class="btn btn-primary w-100">Lihat Detail</a>
+                        <a href="{{ route('employee.violation.class-point.detail', ['classroom' => $classroom->id]) }}"
+                            class="btn btn-primary w-100">Lihat Detail</a>
                     </div>
                 </div>
             </div>
