5337028f3d20d7fc47fbe64bab1e784e8bc917c8
nesaathussholeha <nesaathussholeha@gmail.com>
2024-09-25 14:36:34 +0700

    update modal edit mata pelajaran

--- resources/views/school/pages/subjects/widgets/modal-update-subject.blade.php
+++ resources/views/school/pages/subjects/widgets/modal-update-subject.blade.php
@@ -1,4 +1,3 @@
-
 <div class="modal fade" id="modal-edit" tabindex="-1" aria-labelledby="tambahPelajaran" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
@@ -12,19 +11,15 @@
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="">Mata Pelajaran <span class="text-danger">*</span></label>
-                        <input type="text" id="name-edit" name="name" class="form-control" placeholder="Masukan nama mata pelajaran">
+                        <input type="text" id="name-edit" name="name" class="form-control"
+                            placeholder="Masukan nama mata pelajaran">
                         @error('name', 'update')
                             <div class="text-danger error-edit">
                                 <small>{{ $message }}</small>
                             </div>
                         @enderror
                     </div>
-                    {{-- <div class="mb-3">
-                        <label for="">Kagamaan</label>
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch" id="check">
-                        </div>
-                    </div> --}}
+
                     <div class="mb-3">
                         <label class="mb-2" for="category">Kategori<span class="text-danger ms-1">*</span></label>
                         <select id="category-edit" name="category" class="form-select mb-3">
@@ -33,9 +28,10 @@
                         </select>
                     </div>
 
-                    <div id="religion-field-edit" class="mb-3" style="display: none;">
-                        <select id="religion-edit" name="religion_id" class="form-select form-select mb-3">
-                            <option value="">Pilih agama <span class="text-danger">*</span></option>
+                    <div id="religion-field-edit" class="mb-3" style="display: none;"> <label class="mb-2"
+                            for="category">Agama<span class="text-danger ms-1"></span></label>
+                        <select id="religion-field-edit" name="religion_id" class="form-select form-select mb-3">
+                            <option value="">Pilih agama <span class="text-danger"></span></option>
                             @foreach ($religions as $religion)
                                 <option value="{{ $religion->id }}">{{ $religion->name }}</option>
                             @endforeach
