fae4ba3fb801aee67b628e7a1c433edde9846ec6
nesaathussholeha <nesaathussholeha@gmail.com>
2024-09-26 08:26:04 +0700

    Update alpha-tab.blade.php

--- resources/views/school/pages/dashboard/panes/student-tab/alpha-tab.blade.php
+++ resources/views/school/pages/dashboard/panes/student-tab/alpha-tab.blade.php
@@ -1,5 +1,5 @@
-<div class="table-responsive rounded-2 mb-4">
-    <table class="table border text-nowrap customize-table mb-0 align-middle text-center" style="max-height: 400px; overflow-y: auto;">
+<div class="table-responsive rounded-2 mb-4" style="max-height: 400px; overflow-y: auto;">
+    <table class="table border text-nowrap customize-table mb-0 align-middle text-center">
         <thead class="text-dark fs-4">
             <tr class="">
                 <th class="fs-4 fw-semibold mb-0" style="background-color: #5D87FF; color: white">No</th>
