8f7e88ae196066d46b8aa7068bc3d8b4c9c98fe0
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-10-22 21:05:17 +0700

    Update AttendanceController.php

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -77,8 +77,8 @@ class AttendanceController extends Controller
                             $failedStore[] = $attendance['model_id'];
                         }
                     } else {
+                        dd($attendance);
                         $updated = $this->attendance->updateWithAttribute(['model_id' => $attendance['model_id'], 'model_type' => $attendance['model_type'], 'created_at' => $attendance['created_at']], $attendance);
-                        dd($updated);
                         if (!$updated) {
                             $failedStore[] = $attendance['model_id'];
                         } else {
