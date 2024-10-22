c7dcad7939b65a5baf7169bf1e2547442ef700a8
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-10-22 16:26:12 +0700

    update attendaces

--- app/Services/AttendanceService.php
+++ app/Services/AttendanceService.php
@@ -57,7 +57,6 @@ class AttendanceService
     }
     public function insert($request, $rule, $date): mixed
     {
-        // dd($date);
         $attendances = collect($request->attendances);
 
         $students = [];
@@ -73,11 +72,11 @@ class AttendanceService
             $studentRule = $rule['student'][0];
             $teacherRule = $rule['teacher'][0];
 
-            $time = Carbon::createFromFormat('H:i:s', $attendance['time']);
-            $rfid = $rfids->where('id', $attendance['id'])->first();
+            $time = Carbon::createFromFormat('H:i:s', $attendance->time);
+            $rfid = $rfids->where('id', $attendance->id)->first();
 
             if ($rfid) {
-                if ($attendance['type'] == RoleEnum::STUDENT->value) {
+                if ($attendance->type == RoleEnum::STUDENT->value) {
                     // Student rules
                     $checkinEnd = Carbon::create($studentRule->checkin_end);
                     $checkoutStart = Carbon::create($studentRule->checkout_start);
@@ -139,7 +138,7 @@ class AttendanceService
                     }
                 }
             } else {
-                $invalidAttendances[] = $attendance['id'];
+                $invalidAttendances[] = $attendance->id;
                 return [
                     'model_id' => $rfid->model_id ?? null,
                     'invalid' => true
