4a55c0eed3d6aecebbfec75ed23ee81aaaa099b4
nesaathussholeha <nesaathussholeha@gmail.com>
2024-10-23 09:31:32 +0700

    logo header

--- resources/views/school/layouts/header.blade.php
+++ resources/views/school/layouts/header.blade.php
@@ -26,7 +26,21 @@
                 <img src="{{ asset('assets/images/logo/LOGO-HUMMATECH_Hitam.png') }}" class="dark-logo img-fluid" width="150" alt="" />
             </div> --}}
 
-
+            <div class="d-flex align-items-center">
+                <a href="smkn2kraksaan.sch.id" target="_blank">
+                    <img src="{{ asset('assets/images/logo/logo-smkn2kraksaan.png') }}" class="dark-logo img-fluid"
+                        width="20" alt="" />
+                </a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 15 15"
+                    class="mx-2">
+                    <path fill="currentColor"
+                        d="M3.64 2.27L7.5 6.13l3.84-3.84A.92.92 0 0 1 12 2a1 1 0 0 1 1 1a.9.9 0 0 1-.27.66L8.84 7.5l3.89 3.89A.9.9 0 0 1 13 12a1 1 0 0 1-1 1a.92.92 0 0 1-.69-.27L7.5 8.87l-3.85 3.85A.92.92 0 0 1 3 13a1 1 0 0 1-1-1a.9.9 0 0 1 .27-.66L6.16 7.5L2.27 3.61A.9.9 0 0 1 2 3a1 1 0 0 1 1-1c.24.003.47.1.64.27" />
+                </svg>
+                <a href="https://hummatech.com/" target="_blank">
+                    <img src="{{ asset('assets/images/logo/LOGO-HUMMATECH_Hitam.png') }}" class="dark-logo img-fluid"
+                        width="120" alt="" />
+                </a>
+            </div>
 
         </ul>
         <div class="d-block d-lg-none pt-3">
--- resources/views/staff/layouts/header.blade.php
+++ resources/views/staff/layouts/header.blade.php
@@ -21,6 +21,21 @@
                     aria-label="Toggle navigation">
                 </button>
             </div>
+            <div class="d-flex align-items-center">
+                <a href="smkn2kraksaan.sch.id" target="_blank">
+                    <img src="{{ asset('assets/images/logo/logo-smkn2kraksaan.png') }}" class="dark-logo img-fluid"
+                        width="20" alt="" />
+                </a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 15 15"
+                    class="mx-2">
+                    <path fill="currentColor"
+                        d="M3.64 2.27L7.5 6.13l3.84-3.84A.92.92 0 0 1 12 2a1 1 0 0 1 1 1a.9.9 0 0 1-.27.66L8.84 7.5l3.89 3.89A.9.9 0 0 1 13 12a1 1 0 0 1-1 1a.92.92 0 0 1-.69-.27L7.5 8.87l-3.85 3.85A.92.92 0 0 1 3 13a1 1 0 0 1-1-1a.9.9 0 0 1 .27-.66L6.16 7.5L2.27 3.61A.9.9 0 0 1 2 3a1 1 0 0 1 1-1c.24.003.47.1.64.27" />
+                </svg>
+                <a href="https://hummatech.com/" target="_blank">
+                    <img src="{{ asset('assets/images/logo/LOGO-HUMMATECH_Hitam.png') }}" class="dark-logo img-fluid"
+                        width="120" alt="" />
+                </a>
+            </div>
         </ul>
         <div class="d-block d-lg-none">
             <img src="{{ asset('assets/images/logo/logo-primadona1.png') }}" width="180" class="dark-logo pt-3">
@@ -107,8 +122,9 @@
             <button class="navbar-toggler p-0 border-0" type="button" id="drop1" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 <div class="">
-                    <img src="{{ Auth::user()->image ? asset('storage/' . Auth::user()->image) : asset('assets/images/default-user.jpeg') }}" class="rounded-circle user-profile"
-                        style="object-fit: cover" width="35" height="35" alt="" />
+                    <img src="{{ Auth::user()->image ? asset('storage/' . Auth::user()->image) : asset('assets/images/default-user.jpeg') }}"
+                        class="rounded-circle user-profile" style="object-fit: cover" width="35" height="35"
+                        alt="" />
                 </div>
             </button>
             <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-down" style="width: 320px;"
@@ -118,8 +134,9 @@
                         <h5 class="mb-0 fs-5 fw-semibold">{{ auth()->user()->name }} Profile</h5>
                     </div>
                     <div class="d-flex align-items-center py-9 mx-7 border-bottom">
-                        <img src="{{ Auth::user()->image ? asset('storage/' . Auth::user()->image) : asset('assets/images/default-user.jpeg') }}" class="rounded-circle user-profile"
-                            style="object-fit: cover" width="80" height="80" alt="" />
+                        <img src="{{ Auth::user()->image ? asset('storage/' . Auth::user()->image) : asset('assets/images/default-user.jpeg') }}"
+                            class="rounded-circle user-profile" style="object-fit: cover" width="80"
+                            height="80" alt="" />
                         <div class="ms-3">
                             <h5 class="mb-1 fs-3 username">{{ auth()->user()->name }}</h5>
                             <span class="mb-1 d-block text-dark role">{{ auth()->user()->name }}</span>
