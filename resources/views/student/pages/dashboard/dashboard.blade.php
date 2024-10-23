7a16119fbd3d4935d0a76ccecdd2d01cf0fae0c3
ArdianS18 <ardiansupriadi464@gmail.com>
2024-10-23 14:48:39 +0700

    Update dashboard.blade.php

--- resources/views/student/pages/dashboard/dashboard.blade.php
+++ resources/views/student/pages/dashboard/dashboard.blade.php
@@ -141,7 +141,7 @@
                                         <td>{{ $data->checkin == null ? '-' : \Carbon\Carbon::parse($data->checkin)->format('H:i') }}</td>
                                         <td>{{ $data->checkout == null ? '-' : \Carbon\Carbon::parse($data->checkout)->format('H:i') }}</td>
                                         <td>
-                                            <span class="mb-1 badge font-medium {{$data->status == 'present' ? 'bg-light-success text-success' : ($data->status == 'permit' ? 'bg-light-warning text-warning' : 'bg-light-danger text-danger') }}">{{ $data->status->label() }}</span>
+                                            <span class="mb-1 badge font-medium {{ $data->status->color() }}">{{ $data->status->label() }}</span>
                                         </td>
                                     </tr>
                                 @empty
