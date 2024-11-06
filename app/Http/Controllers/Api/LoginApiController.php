cb993d130bcf8f59b7cbd7211187bb514588a42f
ArdianS18 <ardiansupriadi464@gmail.com>
2024-11-06 09:03:33 +0700

    Update LoginApiController.php

--- app/Http/Controllers/Api/LoginApiController.php
+++ app/Http/Controllers/Api/LoginApiController.php
@@ -31,7 +31,7 @@ class LoginApiController extends Controller
                         'role' => $user->roles->first()->name,
                         'password' => $user->password,
                         'image' => $user->roles->first()->name == 'student' ? ($user->student->image ? asset($fullDomain.'/storage/'.$user->student->image) : asset($fullDomain.'/public/admin_assets/dist/images/profile/user-1.jpg')) : ($user->employee->image ? asset($fullDomain.'storage/'.$user->employee->image) : asset($fullDomain.'/public/admin_assets/dist/images/profile/user-1.jpg')),
-                        'point' => $user->roles->first()->name == 'student' ? $user->student->point : '-',
+                        'point' => $user->roles->first()->name == 'student' ? $user->student->point : 0,
                     ],
                 ]);
             } else {
