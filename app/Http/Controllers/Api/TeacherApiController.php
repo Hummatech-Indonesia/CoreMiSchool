bbf8e316af4c1eeb265a74415e8ffcc419c94c6b
ArdianS18 <ardiansupriadi464@gmail.com>
2024-11-20 13:18:00 +0700

    Update TeacherApiController.php

--- app/Http/Controllers/Api/TeacherApiController.php
+++ app/Http/Controllers/Api/TeacherApiController.php
@@ -60,7 +60,7 @@ class TeacherApiController extends Controller
 
     public function today_lesson_schedule(User $user)
     {
-        $teacherSchedules = $this->lessonSchedule->whereTeacher(auth()->user()->id, today());
+        $teacherSchedules = $this->lessonSchedule->whereTeacher($user->id, today());
         return response()->json(['status' => 'success', 'message' => "Berhasil mengambil data",'code' => 200,
         'lesson_schedule_dashboard' => LessonScheduleResource::collection($teacherSchedules->take(5)),
         'lesson_schedule_all' => LessonScheduleResource::collection($teacherSchedules),
