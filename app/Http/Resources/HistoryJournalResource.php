eca6a2b49cb4d3155e47e8d7700bdd0bb94021b8
ArdianS18 <ardiansupriadi464@gmail.com>
2024-11-20 14:36:31 +0700

    Update HistoryJournalResource.php

--- app/Http/Resources/HistoryJournalResource.php
+++ app/Http/Resources/HistoryJournalResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\AttendanceEnum;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,6 +21,9 @@ class HistoryJournalResource extends JsonResource
             'title' => $this->title,
             'description' => $this->description,
             'date' => $this->date,
+            'count_alpha' => $this->attendanceJournals->where('status', AttendanceEnum::ALPHA)->count(),
+            'count_sick' => $this->attendanceJournals->where('status', AttendanceEnum::SICK)->count(),
+            'count_permit' => $this->attendanceJournals->where('status', AttendanceEnum::PERMIT)->count(),
         ];
     }
 }
