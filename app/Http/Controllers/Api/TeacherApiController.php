9fa82fa039fc9bcbc8c98276388ae78e1b150263
ArdianS18 <ardiansupriadi464@gmail.com>
2024-11-20 11:40:35 +0700

    Update TeacherApiController.php

--- app/Http/Controllers/Api/TeacherApiController.php
+++ app/Http/Controllers/Api/TeacherApiController.php
@@ -41,15 +41,7 @@ class TeacherApiController extends Controller
         $history_attendance = $this->attendance->whereUser($employee->id, 'App\Models\Employee');
 
         return response()->json(['status' => 'success', 'message' => "Berhasil mengambil data",'code' => 200,
-        // 'attendance_today' => [
-        //     'day' => $todayAttendance->created_at == null ? '-' : Carbon::parse($todayAttendance->created_at)->translatedFormat('l'),
-        //     'date' => $todayAttendance->created_at == null ? '-' : Carbon::parse($todayAttendance->created_at)->translatedFormat('d'),
-        //     'month' => $todayAttendance->created_at == null ? '-' : Carbon::parse($todayAttendance->created_at)->translatedFormat('M'),
-        //     'date_complate' => $todayAttendance->created_at == null ? '-' : Carbon::parse($todayAttendance->created_at)->translatedFormat('l, j F Y'),
-        //     'check_in' => $todayAttendance->checkin == null ? '-' : \Carbon\Carbon::parse($todayAttendance->checkin)->format('H:i'),
-        //     'check_out' => $todayAttendance->checkout == null ? '-' : \Carbon\Carbon::parse($todayAttendance->checkout)->format('H:i'),
-        //     'status' => $todayAttendance->status == null ? '-' : $todayAttendance->status->label(),
-        // ],
+        'attendance_today' =>  HistoryAttendanceResource::collection($todayAttendance),
         'attendance_history' => HistoryAttendanceResource::collection($history_attendance),
         ]);
     }
