d21e78cf09750b640bda69985570e84e84ea7fe8
13Farahamalia <farahamalia328@gmail.com>
2024-12-11 23:11:54 +0700

    Update LessonScheduleRepository.php

--- app/Contracts/Repositories/LessonScheduleRepository.php
+++ app/Contracts/Repositories/LessonScheduleRepository.php
@@ -63,7 +63,6 @@ class LessonScheduleRepository extends BaseRepository implements LessonScheduleI
 
     public function whereTeacher(mixed $id, mixed $day): mixed
     {
-        dd($day->format('l'));
         return $this->model->query()
             ->whereRelation('teacherSubject.employee.user', 'id', $id)
             ->with('teacherJournals', function ($query) use ($day) {
