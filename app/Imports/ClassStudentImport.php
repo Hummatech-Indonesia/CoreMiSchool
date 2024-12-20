ac31eb4b9b46aeded5fcb1ef43293646fbae8123
ArdianS18 <ardiansupriadi464@gmail.com>
2024-12-20 08:18:34 +0700

    Update ClassStudentImport.php

--- app/Imports/ClassStudentImport.php
+++ app/Imports/ClassStudentImport.php
@@ -52,7 +52,8 @@ class ClassStudentImport implements ToModel, WithHeadingRow, WithEvents
 
             $studentId = "";
             $user->assignRole(RoleEnum::STUDENT->value);
-            $birthDate = $row['tanggal_lahir'] ? Carbon::instance(Date::excelToDateTimeObject($row['tanggal_lahir'])) : null;
+            // $birthDate = $row['tanggal_lahir'] ? Carbon::instance(Date::excelToDateTimeObject($row['tanggal_lahir'])) : null;
+            $birthDate = now();
             // $birthDate = $row['tanggal_lahir'] ? Carbon::createFromFormat('Y-m-d', $row['tanggal_lahir'])->format('Y-m-d') : null;
 
             $data = [
