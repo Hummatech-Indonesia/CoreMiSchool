d8f223c4183214c69785bd1565f001077deb965d
Alfian <112760996+alfian8752@users.noreply.github.com>
2025-01-08 22:51:52 +0700

    update command

--- app/Console/Commands/CreateAttendanceCommand.php
+++ app/Console/Commands/CreateAttendanceCommand.php
@@ -34,44 +34,40 @@ class CreateAttendanceCommand extends Command
     {
         $day = strtolower(now()->format('l'));
         $date = date('Y-m-d');
-        // info($date);
-        // info(Attendance::whereDate('created_at', $date)->where('model_type', 'App\Models\ClassroomStudent')->first());
-        // return;
+        $classroomStudents = ClassroomStudent::with(['classroom.lessonSchedule' => function ($query) use ($day) {
+            $query->where('day', $day);
+        }])
+            ->whereRelation('classroom.schoolYear', function ($query) {
+                $query->where('active', 1);
+            })->whereHas('student.modelHasRfid')->get();
+
+        $attendanceStudent = $classroomStudents->map(function ($student) use ($day) {
+            return [
+                'point' => 13,
+                'model_type' => "App\Models\ClassroomStudent",
+                'model_id' => $student->id,
+                'created_at' => now(),
+                'status' => AttendanceEnum::ALPHA->value
+            ];
+        })->toArray();
         if (!Attendance::whereDate('created_at', $date)->where('model_type', 'App\Models\ClassroomStudent')->first()) {
-            $classroomStudents = ClassroomStudent::with(['classroom.lessonSchedule' => function ($query) use ($day) {
-                $query->where('day', $day);
-            }])
-                ->whereRelation('classroom.schoolYear', function ($query) {
-                    $query->where('active', 1);
-                })->whereHas('student.modelHasRfid')->get();
-
-            $attendanceStudent = $classroomStudents->map(function ($student) use ($day) {
-                return [
-                    'point' => 13,
-                    'model_type' => "App\Models\ClassroomStudent",
-                    'model_id' => $student->id,
-                    'created_at' => now(),
-                    'status' => AttendanceEnum::ALPHA->value
-                ];
-            })->toArray();
         }
 
+        $teachers = Employee::whereHas('modelHasRfid')
+            ->where('status', RoleEnum::TEACHER->value)
+            ->get();
+
+        $attendanceTeacher = $teachers->map(function ($teacher) {
+            return [
+                'point' => 13,
+                'model_type' => "App\Models\Employee",
+                'model_id' => $teacher->id,
+                'created_at' => now(),
+                'status' => AttendanceEnum::ALPHA->value
+            ];
+        })->toArray();
         if (!Attendance::whereDate('created_at', $date)->where('model_type', 'App\Models\Employee')->first()) {
-            $teachers = Employee::whereHas('modelHasRfid')
-                ->where('status', RoleEnum::TEACHER->value)
-                ->get();
-
-            $attendanceTeacher = $teachers->map(function ($teacher) {
-                return [
-                    'point' => 13,
-                    'model_type' => "App\Models\Employee",
-                    'model_id' => $teacher->id,
-                    'created_at' => now(),
-                    'status' => AttendanceEnum::ALPHA->value
-                ];
-            })->toArray();
         }
-        // $stored = $classroomStudents->attendance()->insert(['status' => 'alpha']);
         $attendanceData = array_merge($attendanceTeacher ?? [], $attendanceStudent ?? []);
 
         info($attendanceData);
